017734f92be7f56b4a9ed28b445857b9df017236
Danny Bof <danny.bof@gmail.com>
2018-11-17 20:43:09 -0800

    Added billing and client side app. All up to section 9.113

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { FETCH_USER } from './types';
+import axios from "axios";
+import { FETCH_USER } from "./types";
 
 // export const fetchUser = () => {
 //   return function(dispatch) {
@@ -11,7 +11,13 @@ import { FETCH_USER } from './types';
 
 // Equivalent to the commented one above
 export const fetchUser = () => async dispatch => {
-    const res  = await axios.get('/api/current_user');
+  const res = await axios.get("/api/current_user");
 
-    dispatch({ type: FETCH_USER, payload: res.data });
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
